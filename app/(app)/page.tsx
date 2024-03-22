66f8e51f731369cb2f82332e5f11ccb2147a40de
Santiago Rodriguez Villanuev <ssyt21ss@gmail.com>
2024-03-22 18:26:19 +0000

    Fixed width error

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -7,13 +7,13 @@ export default async function Home() {
   const randomSlug = generateRandomString();
   return (
     <div className="container relative">
-      <div className="flex flex-col h-[calc(100vh-4rem)] w-full flex-1 items-center justify-center">
-        <div className="flex flex-col gap-y-3 w-full h-full max-w-md items-center justify-center">
-          <h1 className="font-bold text-3xl">Linkify</h1>
-          <p className="text-center text-muted-foreground text-sm">
+      <div className="flex h-[calc(100vh-4rem)] w-full flex-1 flex-col items-center justify-center">
+        <div className="flex h-full w-full max-w-md flex-col items-center justify-center gap-y-3">
+          <h1 className="text-3xl font-bold">Linkify</h1>
+          <p className="text-center text-sm text-muted-foreground">
             Free, unlimited url shortener.
           </p>
-          <div className="max-h-96 pb-10">
+          <div className="max-h-96 w-full pb-10">
             <CreateLinkForm randomSlug={randomSlug} session={session} />
           </div>
         </div>
--- components/forms/create-link.tsx
+++ components/forms/create-link.tsx
@@ -97,249 +97,245 @@ export function CreateLinkForm({
   }
   const setOpen = useFeaturesDialog((state) => state.setOpen);
   return (
-    <>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full pb-2 grid gap-y-3"
-        >
-          <FormField
-            control={form.control}
-            name="url"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>URL</FormLabel>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid w-full gap-y-3 pb-2"
+      >
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL</FormLabel>
+              <FormControl>
+                <Input placeholder="https://example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="slug"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Slug</FormLabel>
+              <div className="relative">
                 <FormControl>
-                  <Input placeholder="https://example.com" {...field} />
+                  <Input placeholder="my-slug" {...field} />
                 </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="slug"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Slug</FormLabel>
-                <div className="relative">
-                  <FormControl>
-                    <Input placeholder="my-slug" {...field} />
-                  </FormControl>
-                  <span className="absolute flex items-center justify-center right-3 top-1/2 -translate-y-1/2">
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Button
-                            type="button"
-                            variant="ghost"
-                            size="icon"
-                            className="size-6"
-                          >
-                            <Info className="h-4 w-4" />
-                          </Button>
-                        </TooltipTrigger>
-                        <TooltipContent className="mr-2 md:mr-0">
-                          This could not be modified.
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </span>
-                </div>
-                <FormDescription>
-                  Your Linkify URL will be: <br />
-                  {process.env.NEXT_PUBLIC_APP_URL}/l/{slugify(field.value)}
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1" className="border-none">
-              <AccordionTrigger className="py-0 rounded-lg space-x-2">
-                <span className="text-sm font-medium leading-6 text-foreground">
-                  Advanced Options
+                <span className="absolute right-3 top-1/2 flex -translate-y-1/2 items-center justify-center">
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button
+                          type="button"
+                          variant="ghost"
+                          size="icon"
+                          className="size-6"
+                        >
+                          <Info className="h-4 w-4" />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent className="mr-2 md:mr-0">
+                        This could not be modified.
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
                 </span>
-              </AccordionTrigger>
-              <AccordionContent className="space-y-2 first:pt-5">
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center justify-between">
-                    <Label htmlFor="custom-expires-date">Expire Date</Label>
-                    <Switch
-                      id="custom-expires-date"
-                      checked={customExpireDate}
-                      onCheckedChange={(v) => {
-                        if (!session) {
-                          setOpen(true);
-                          return;
-                        }
-                        setCustomExpireDate(v);
-                      }}
-                    />
-                  </div>
-                  {customExpireDate && (
-                    <FormField
-                      control={form.control}
-                      name="expires_at"
-                      render={({ field }) => (
-                        <FormItem>
-                          <Popover>
-                            <PopoverTrigger asChild>
-                              <FormControl>
-                                <Button
-                                  variant={"outline"}
-                                  className={cn(
-                                    "w-full pl-3 text-left font-normal",
-                                    !field.value && "text-muted-foreground"
-                                  )}
-                                >
-                                  {field.value ? (
-                                    format(field.value, "D MMMM YYYY, HH:mm A")
-                                  ) : (
-                                    <span>Pick a date</span>
-                                  )}
-                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                </Button>
-                              </FormControl>
-                            </PopoverTrigger>
-                            <PopoverContent
-                              className="w-auto p-0 space-y-1"
-                              align="start"
-                            >
-                              <Calendar
-                                mode="single"
-                                selected={field.value}
-                                onSelect={field.onChange}
-                                disabled={(date: Date) =>
-                                  date < new Date() ||
-                                  (!session && date > addDay(new Date(), 7))
-                                }
-                                initialFocus
-                              />
-                              <Input
-                                type="time"
-                                className="mt-2"
-                                // take locale date time string in format that the input expects (24hr time)
-                                value={field.value?.toLocaleTimeString([], {
-                                  hourCycle: "h23",
-                                  hour: "2-digit",
-                                  minute: "2-digit",
-                                })}
-                                // take hours and minutes and update our Date object then change date object to our new value
-                                onChange={(selectedTime) => {
-                                  const currentTime = field.value;
-                                  currentTime?.setHours(
-                                    parseInt(
-                                      selectedTime.target.value.split(":")[0]
-                                    ),
-                                    parseInt(
-                                      selectedTime.target.value.split(":")[1]
-                                    ),
-                                    0
-                                  );
-                                  field.onChange(currentTime);
-                                }}
-                              />
-                            </PopoverContent>
-                          </Popover>
-                        </FormItem>
-                      )}
-                    />
-                  )}
+              </div>
+              <FormDescription>
+                Your Linkify URL will be: <br />
+                {process.env.NEXT_PUBLIC_APP_URL}/l/{slugify(field.value)}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1" className="border-none">
+            <AccordionTrigger className="space-x-2 rounded-lg py-0">
+              <span className="text-sm font-medium leading-6 text-foreground">
+                Advanced Options
+              </span>
+            </AccordionTrigger>
+            <AccordionContent className="space-y-2 first:pt-5">
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center justify-between">
+                  <Label htmlFor="custom-expires-date">Expire Date</Label>
+                  <Switch
+                    id="custom-expires-date"
+                    checked={customExpireDate}
+                    onCheckedChange={(v) => {
+                      if (!session) {
+                        setOpen(true);
+                        return;
+                      }
+                      setCustomExpireDate(v);
+                    }}
+                  />
                 </div>
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center justify-between">
-                    <Label htmlFor="password-protected">
-                      Password Protected
-                    </Label>
-                    <Switch
-                      id="password-protected"
-                      checked={passwordProtected}
-                      onCheckedChange={(v) => {
-                        if (!session) {
-                          setOpen(true);
-                          return;
-                        }
-                        setPasswordProtected(v);
-                      }}
-                    />
-                  </div>
-                  {passwordProtected && (
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormControl>
-                            <Input
-                              type="password"
-                              placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-                              {...field}
+                {customExpireDate && (
+                  <FormField
+                    control={form.control}
+                    name="expires_at"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-full pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "D MMMM YYYY, HH:mm A")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent
+                            className="w-auto space-y-1 p-0"
+                            align="start"
+                          >
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date: Date) =>
+                                date < new Date() ||
+                                (!session && date > addDay(new Date(), 7))
+                              }
+                              initialFocus
                             />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  )}
+                            <Input
+                              type="time"
+                              className="mt-2"
+                              // take locale date time string in format that the input expects (24hr time)
+                              value={field.value?.toLocaleTimeString([], {
+                                hourCycle: "h23",
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              })}
+                              // take hours and minutes and update our Date object then change date object to our new value
+                              onChange={(selectedTime) => {
+                                const currentTime = field.value;
+                                currentTime?.setHours(
+                                  parseInt(
+                                    selectedTime.target.value.split(":")[0],
+                                  ),
+                                  parseInt(
+                                    selectedTime.target.value.split(":")[1],
+                                  ),
+                                  0,
+                                );
+                                field.onChange(currentTime);
+                              }}
+                            />
+                          </PopoverContent>
+                        </Popover>
+                      </FormItem>
+                    )}
+                  />
+                )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center justify-between">
+                  <Label htmlFor="password-protected">Password Protected</Label>
+                  <Switch
+                    id="password-protected"
+                    checked={passwordProtected}
+                    onCheckedChange={(v) => {
+                      if (!session) {
+                        setOpen(true);
+                        return;
+                      }
+                      setPasswordProtected(v);
+                    }}
+                  />
                 </div>
-                <FormField
-                  control={form.control}
-                  name="allowUnauthenticated"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-row items-center justify-between rounded-lg">
-                      <div className="space-y-0.5">
-                        <FormLabel className="">
-                          Allow unauthenticated users
-                        </FormLabel>
-                        <FormDescription className="text-xs">
-                          Users will be able to access your link without an
-                          account.
-                        </FormDescription>
-                      </div>
-                      <FormControl>
-                        <Switch
-                          checked={field.value}
-                          onCheckedChange={() => {
-                            if (!session) {
-                              setOpen(true);
-                              return;
-                            }
-                            field.onChange;
-                          }}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+                {passwordProtected && (
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+              </div>
+              <FormField
+                control={form.control}
+                name="allowUnauthenticated"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-center justify-between rounded-lg">
+                    <div className="space-y-0.5">
+                      <FormLabel className="">
+                        Allow unauthenticated users
+                      </FormLabel>
+                      <FormDescription className="text-xs">
+                        Users will be able to access your link without an
+                        account.
+                      </FormDescription>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={() => {
+                          if (!session) {
+                            setOpen(true);
+                            return;
+                          }
+                          field.onChange;
+                        }}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
 
+        <Button
+          disabled={form.formState.isSubmitting}
+          type="submit"
+          className="w-full"
+        >
+          {form.formState.isSubmitting && (
+            <Loader2 className="size-4 animate-spin" />
+          )}
+          Create Link
+        </Button>
+
+        {!session && (
           <Button
-            disabled={form.formState.isSubmitting}
-            type="submit"
+            type="button"
+            onClick={() => setOpen(true)}
+            variant="link"
             className="w-full"
           >
-            {form.formState.isSubmitting && (
-              <Loader2 className="size-4 animate-spin" />
-            )}
-            Create Link
+            <Gem className="size-4" />
+            Unlock all features
           </Button>
-
-          {!session && (
-            <Button
-              type="button"
-              onClick={() => setOpen(true)}
-              variant="link"
-              className="w-full"
-            >
-              <Gem className="size-4" />
-              Unlock all features
-            </Button>
-          )}
-        </form>
-      </Form>
-    </>
+        )}
+      </form>
+    </Form>
   );
 }
